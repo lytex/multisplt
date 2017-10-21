2c98fab7e5b6f198cf268f9ffdb093d13fd8a937
lytex <32138262+lytex@users.noreply.github.com>
2017-10-21 19:09:45 +0200

    Add files via upload
    Translated to english

--- multisplt.py
+++ multisplt.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
 # coding=UTF-8
 
-# import argparse
 import sys
 import re
 import os
 import math
 
 
-# Formatos que tiene que reconocer
-# mm:ss con mm > 59, mm > 99
-# hh:mm:ss con mm <= 59, ss <= 59
+# Time formats
+# mm:ss with mm > 59, mm > 99
+# hh:mm:ss with mm <= 59, ss <= 59
 
-# Formato de argumentos del script de python
+# Arguments format
 # 01 52nd Street Theme 00:11 02 A Night in Tunisia 05:03 03 All The Things You Are 08:07 04 Embraceable You 15:21
 # 00:00:00 01 Serenade Grotesque 00:03:20 02 Menuet Antique 00:09:31 03 Pavane Pour Une infante defunte 00:15:55 04 Jeux D'eau
-# Los nombres de canciones no tienen que estar entre comillas y pueden ir antes o después de los títulos
-# A las regex les da igual si el título está antes o después del tiempo
 
-# Formato de mp3splt
+# The song names don't have to be between quotes and can be before or after the timestamps (the regex don't care)
+
+# mp3splt format
 # mp3splt foo.mp3  0.30 1.00 2.00 EOF -o @n
 # 1.mp3 0:30 - 1:00
 # 2.mp3 1:00 - 2:00
@@ -28,7 +27,7 @@ import math
 def toMinSec(time):
     splited = re.split(":", time)
     if (len(splited) is 2):
-        # No hay que convertir
+        # No need to convert
         return (splited[0]+"."+splited[1])
     elif (len(splited) is 3):
         minutes = int(splited[0])*60 + int(splited[1])
@@ -37,13 +36,11 @@ def toMinSec(time):
     else:
         return None
 
-# TODO si el argumento de entrada tiene ' hay que cerrarlas aunque luego el programa no las interpreta. Esto ocurre antes de que se empieze el script
+# TODO if the argument has ' quotes must be closed even when the script doesn't use them. This happens before the script runs
 
 inputfile = sys.argv[1]
-#del sys.argv[0] # Nombre del programa
-#del sys.argv[0] # Archivo de entrada
 argv = ' '.join(sys.argv[2:len(sys.argv)])
-# Quita lo de arriba y lo convierte en str separado por ' '
+# Removes name of the program argv[0] and input file argv[1] and converts it to srt separated by ' '
 
 
 # \d+:\d{2} -> mm:ss
@@ -56,17 +53,19 @@ num_time = len(arg_time)
 
 arg_name = re.split(time_regex, argv)
 
+
+# arg_name has some empty strings entries we need to remove
 try:
-    # Sólo elimina un '' de cada vez
+    # Only eliminates one '' each time
     for i in range(0, len(arg_name)):
-        arg_name.remove('') # Si no lo encuentra lanza un error
+        arg_name.remove('') # If it doesn't find it throws an error
 except ValueError:
     pass
 
 
 num_name = len(arg_name)
 
-# Siempre hay un espacio al final de arg_name[0] y para todos los siguientes hay espacios al principio y al final
+# There's always a space at the end of arg_name[0] y the rest have spaces both at the end and the beggining
 
 temp = arg_name[0][0:len(arg_name[0])-1]
 arg_name[0] = temp
@@ -75,12 +74,10 @@ for i in range(1, num_name):
     temp = arg_name[i][1:len(arg_name[i])-1]
     arg_name[i] = temp
 
-#print(arg_name)
-#print (arg_time)
 
 
-# TODO comprobar que num_name = num_time + 1
-# Puede ser que el tiempo inicial esté implícito y num_name = num_time+2
+# TODO check that nun_name = num_time + 1
+# Initial timestamp may be implicit and num_name = num_time + 2
 
 
 
@@ -93,7 +90,7 @@ if (num_name == num_time):
 
 else:
 
-    sys.exit("El número de tiempos y nombres no coindide")
+    sys.exit("The number of names and timestamps doesn't match")
 
 
 
@@ -101,7 +98,7 @@ os.system("mp3splt "+mp3args)
 
 pad = math.floor(math.log10(num_name))+1
 
-# Los nombres de archivo seran str(i).zfill(pad)
+# The mp3splt name files will be str(i).zfill(pad)
 
 for i in range(1, num_name+1):
     print (str(i).zfill(pad)+".mp3")
